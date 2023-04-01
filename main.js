23597c36f6438639251e2d7969f37cd1a8ec2c76
2ujin <suddenlydisappear200@gmail.com>
2023-04-01 17:27:58 +0900

    feat: add main pagex

--- main.js
+++ main.js
@@ -1,3 +1,29 @@
+let contacts = [
+  {
+    name: "Harry",
+    surname: "Potter  ",
+    phone_number: "20123123124",
+    address: "seoul, korea",
+  },
+  {
+    name: "Ron ",
+    surname: "Weasley",
+    phone_number: "20123123211",
+    address: "seoul, korea",
+  },
+  {
+    name: "Hermione  ",
+    surname: "Granger",
+    phone_number: "20123112312",
+    address: "seoul, korea",
+  },
+];
+
 window.onload = function () {
-  console.log("hello world");
+  const div = document.getElementById("user-wrapper");
+  for (let item of contacts) {
+    div.innerHTML += `<div class='name'>${
+      item.name + " " + item.surname
+    }</div><div class='phone'>${item.phone_number}</div>  <hr /> `;
+  }
 };
